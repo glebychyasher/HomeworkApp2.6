d29fef6e3f8a295b9e2b0a2b7988d6b90774fcba
glebychyasher <zobningleb01@icloud.com>
2023-10-08 23:58:05 +0300

    Ver 0

--- HomeworkApp6/ViewController.swift
+++ HomeworkApp6/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var userNameTextField: UITextField!
+    
+    private let userName = "Petya"
+    private let password = "PeTyA120901"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        logInButton.layer.cornerRadius = 10
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func forgotUserNameAction() {
+        showAlert(with: "Oops!", and: "Your user name is \(userName)")
+    }
+    
+    @IBAction func forgotPasswordAction() {
+        showAlert(with: "Oops!", and: "Your password is \(password)")
+    }
+    
+    
+    private func showAlert(with title: String, and message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction) //добавляем кнопку к alert
+        present(alert, animated: true)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super .touchesBegan(touches, with: event)
+    }
 }
+
+
 
